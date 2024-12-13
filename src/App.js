784640de96cffd8b2509a47d0bf6bb1bbca65a43
c0llxl <collosalmango@gmail.com>
2024-12-14 00:16:20 +0700

    React Dasar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+import About from "./components/About.js";
+import 'bulma/css/bulma.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<Router>
+<div>
+<div className="container">
+<div className="columns">
+<div className="column is-half is-offset-one-quarter">
+<Routes>
+<Route path="/" element={<ProductList />} />
+<Route path="/add" element={<AddProduct />} />
+<Route path="/edit/:id" element={<EditProduct />} />
+</Routes>
+</div>
+</div>
+</div>
+</div>
+</Router>
+)
 }
-
 export default App;
